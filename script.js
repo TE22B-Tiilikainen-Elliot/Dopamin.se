e3eb70d98939e1ad86420abf1a8d7024d0ef2fd0
TE22B-Tiilikainen-Elliot <elliot.tiilikainenvenstrom@elev.ga.ntig.se>
2023-11-29 14:40:46 +0100

    more videos

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const body = document.querySelector("body");
 const main = document.getElementById("main")
 
 var boxes = 0;
+let video = 0;
+
+const videos = [
+  'https://www.youtube.com/embed/Pt5_GSKIWQM?si=6FtNU6HUs0d1mRIe&autoplay=1'
+  ,'https://www.youtube.com/embed/ChBg4aowzX8?si=BU6f3RM_lBnv_9M8&amp&autoplay=1'
+  ,'https://www.youtube.com/embed/bHTjGKjdUiU?si=-8_79aAHmX9nlbxZ&amp&autoplay=1'
+  ,'https://www.youtube.com/embed/9eqvo0uqpTs?si=TxMTK1E3qFJOUcES&amp&autoplay=1'
+  ];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function Blocks() {
 
@@ -18,8 +30,12 @@ function Blocks() {
     block.className = "blocks";
     block.id = i;
     main.appendChild(block);
+    if (video == videos.length) {
+      video = 0;
+    }
     document.getElementById(i).innerHTML =
-    "<iframe src='https://www.youtube.com/embed/Pt5_GSKIWQM?si=6FtNU6HUs0d1mRIe&autoplay=1' frameborder='0'></iframe>";
+    "<iframe src='" + videos[video] + "' frameborder='0'></iframe>";
+    video++
   }
 }
 

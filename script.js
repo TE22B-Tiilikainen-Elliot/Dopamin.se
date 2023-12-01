43ea32d009893dec6a390996f6eb9b9b46040007
TE22B-Tiilikainen-Elliot <elliot.tiilikainenvenstrom@elev.ga.ntig.se>
2023-12-01 11:21:19 +0100

    autoplay

--- script.js
+++ script.js
@@ -2,17 +2,16 @@ const body = document.querySelector("body");
 const main = document.getElementById("main")
 
 var boxes = 0;
-let video = 0;
 
 const videos = [
-  'https://www.youtube.com/embed/Pt5_GSKIWQM?si=6FtNU6HUs0d1mRIe&amp&autoplay=1'
-  ,'https://www.youtube.com/embed/ChBg4aowzX8?si=BU6f3RM_lBnv_9M8&amp&autoplay=1'
-  ,'https://www.youtube.com/embed/bHTjGKjdUiU?si=-8_79aAHmX9nlbxZ&amp&autoplay=1'
-  ,'https://www.youtube.com/embed/9eqvo0uqpTs?si=TxMTK1E3qFJOUcES&amp&autoplay=1'
-  ,'https://www.youtube.com/embed/kqFxhfSKZdM?si=OieRKYfFufKGbn92&amp&autoplay=1'
-  ,'https://www.youtube.com/embed/n_Dv4JMiwK8?si=w3VxPbFFq_lEn6nY&amp&autoplay=1'
-  ,'https://www.youtube.com/embed/10gjsgA6fTE?si=onRBAVxvywY0mlUY&amp&autoplay=1'
-  ,'https://www.youtube.com/embed/vVJuMq1CMNo?si=htG_uCsMzJbn9JmX&amp&autoplay=1'
+   'https://www.youtube.com/embed/Pt5_GSKIWQM?si=6FtNU6HUs0d1mRIe'
+  ,'https://www.youtube.com/embed/ChBg4aowzX8?si=BU6f3RM_lBnv_9M8'
+  ,'https://www.youtube.com/embed/bHTjGKjdUiU?si=-8_79aAHmX9nlbxZ'
+  ,'https://www.youtube.com/embed/9eqvo0uqpTs?si=TxMTK1E3qFJOUcES'
+  ,'https://www.youtube.com/embed/kqFxhfSKZdM?si=OieRKYfFufKGbn92'
+  ,'https://www.youtube.com/embed/n_Dv4JMiwK8?si=w3VxPbFFq_lEn6nY'
+  ,'https://www.youtube.com/embed/10gjsgA6fTE?si=onRBAVxvywY0mlUY'
+  ,'https://www.youtube.com/embed/vVJuMq1CMNo?si=htG_uCsMzJbn9JmX'
   ];
 
 function getRandomInt(max) {
@@ -20,7 +19,6 @@ function getRandomInt(max) {
 }
 
 function Blocks() {
-
   if (window.innerWidth < 600) {
     boxes = 2;
   } else {
@@ -34,13 +32,8 @@ function Blocks() {
     block.className = "blocks";
     block.id = i;
     main.appendChild(block);
-    if (video == videos.length) {
-      video = 0;
-    }
     document.getElementById(i).innerHTML =
-    "<iframe src='" + videos[getRandomInt(8)] + "' frameborder='0'></iframe>";
-    
-    video++
+    "<iframe src='" + videos[getRandomInt(8)] + "&autoplay=1&mute=1&loop=1' frameborder='0'></iframe>";
   }
 }
 
